94f36f9ea7a1ed615da0a8f816bbe77e81e3c2d1
magnus stahre <ms@xy.org>
2017-01-12 12:13:17 -0500

    write a file

--- src/main/java/com/pillartechnology/codemash/DC.java
+++ src/main/java/com/pillartechnology/codemash/DC.java
@@ -1,6 +1,14 @@
 package com.pillartechnology.codemash;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class DC {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream("DC.class");
+        try {
+        } finally {
+           fos.close();
+        }
     }
 }
